34b351cab743068d116d4bd1b31d148b88c499e6
tropifly <tropifly@yahoo.com>
2020-12-29 21:53:47 +0100

    correction

--- menu.js
+++ menu.js
@@ -1,7 +1,11 @@
-let button = document.getElementById("reservation");
-
-button.addEventListener("click", function() {
+let feuille = document.getElementById("reservation");
+feuille.addEventListener("click", function() {
     document.location.href = "reservation.html"
+});
+
+let calendrier = document.getElementById("calendrier");
+calendrier.addEventListener("click", function() {
+    document.location.href = "planning.html"
 });
 
 let retour = document.getElementById("retour");
